26f9d3f5173275bb180f00869c27a9d3d80d4852
PauliusMorku <paulius.morku@gmail.com>
2017-11-29 16:35:12 +0100

    Changes before you left

--- state_machine.cpp
+++ state_machine.cpp
@@ -1,4 +1,67 @@
 #include "state_machine.h"
 #include <unistd.h>
 
-// TODO
+void stateMachine::process()
+{
+    enum base {Start,G,GA,GAA,GAAG};
+    base sate;
+
+    output.write(false);
+    switch (sate){
+    case Start:
+        if (input == 'G')
+        {
+            sate = G;
+        }
+        break;
+    case G:
+        if (input == 'A')
+        {
+            sate = GA;
+        }
+        else if (input == 'G')
+        {
+            sate = G;
+        }
+        else
+        {
+            sate = Start;
+        }
+        break;
+    case GA:
+        if (input == 'A')
+        {
+            sate = GAA;
+        }
+        else if (input == 'G')
+        {
+            sate = G;
+        }
+        else
+        {
+            sate = Start;
+        }
+        break;
+    case GAA:
+        if (input == 'G')
+        {
+            sate = GAAG;
+        }
+        else
+        {
+            sate = Start;
+        }
+        break;
+    case GAAG:
+        if (input == 'G')
+        {
+            sate = G;
+        }
+        else
+        {
+            sate = Start;
+        }
+        output.write(true);
+        break;
+    }
+}
--- state_machine.h
+++ state_machine.h
@@ -3,7 +3,24 @@
 
 #include <systemc.h>
 
-// TODO
+SC_MODULE(stateMachine)
+{
+public:
+    sc_in<char> input;
+    sc_in<bool> clk;
+    sc_out<bool> output;
+
+    SC_CTOR(stateMachine): input("input"), clk("clk"), output("output")
+    {
+        SC_METHOD(process);
+        dont_initialize();
+        sensitive << clk.pos();
+    }
+
+private:
+    void process();
+};
+
 
 #endif // FSM_H
 
